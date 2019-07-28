f9434a0b2cf68c507746b06a17be20cd57cd8c29
Bharath-1996 <53391195+Bharath-1996@users.noreply.github.com>
2019-07-28 12:06:03 +0530

    complete user registration and login using asp.net
    First need to create the database call UserDB in sql server and then create a table User as DbModel.user.cs file, you can see the columns in that file. then copy create a folder called DBModel. and Remaining folders such as Model,Controllers,view paste all the files in their respective folders

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.DBModel;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserDBEntities objUserDBEntities = new UserDBEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            UserModel objUserModel = new UserModel();
+            return View(objUserModel);
+        }
+        [HttpPost]
+        public ActionResult Register(UserModel objUserModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User objUser = new User();
+                objUser.FirstName = objUserModel.FirstName;
+                objUser.LastName = objUserModel.LastName;
+                objUser.Email = objUserModel.Email;
+                objUser.Password = objUserModel.Password;
+                objUser.CreatedOn = DateTime.Now;
+                objUserDBEntities.Users.Add(objUser);
+                objUserDBEntities.SaveChanges();
+                objUserModel.message = "successfull registered";
+                return View(objUserModel);
+            } 
+            return View();
+        }
+        public ActionResult Login()
+        {
+            LoginModel objLoginModel = new LoginModel();
+            return View(objLoginModel);
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel objLoginModel)
+        {
+            if(ModelState.IsValid)
+            {
+                if(objUserDBEntities.Users.Where(m => m.Email == objLoginModel.Email && m.Password==objLoginModel.password).FirstOrDefault()==null)
+                {
+                    ModelState.AddModelError("Error", "Email and password is not matching");
+                    return View();
+                }
+                else
+                {
+                    Session["email"] = objLoginModel.Email;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage ="First name is required")]
+        [Display(Name ="first name")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "last name is required")]
+        [Display(Name = "last name")]
+        public string LastName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "password is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "confirm password is required")]
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="password and confirm password is same")]
+        public string Confirmpassword { get; set; }
+        public DateTime CreateOn { get; set; }
+        public string message { get; set; }
+    }
+}
